5ddc35ae51d99867d8e8b5714669d59b0a86a490
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-09-28 12:39:37 -0700

    Add files via upload

--- Product.java
+++ Product.java
@@ -1,1 +1,16 @@
 
+public class Product
+{
+	// Instance Variables
+	public String Name;
+	public double price;
+	public int inventoryQuantity;
+	
+	// methods
+	public String toString()
+	{
+		return " Product Name " + Name + "Cost" + price + " Quantity"+ inventoryQuantity;
+}
+
+
+}
--- SuperStore.java
+++ SuperStore.java
@@ -1,1 +1,21 @@
 
+public class SuperStore
+{
+	public static void main ( String [] args)
+	{
+		Product product1= new Product();
+		product1.Name= "Acer computer";
+		
+		System.out.println(" Product name: " + product1.Name);
+		
+		Product price1= new Product();
+		price1.price= 750.50;
+		
+		System.out.println(" Price: " + price1.price);
+		
+		Product qty= new Product();
+		qty.inventoryQuantity=500;
+		
+		System.out.println(" Quantity " + qty.inventoryQuantity);
+	}
+}
