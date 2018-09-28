e64249a41cb76db05a2d9facfd43efb83b1c7bf4
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-09-28 12:13:01 -0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,25 @@
+/* *****************************************
+ * Class: Car
+ * 
+ * 
+ * ***************************************** */
+ 
+ public class Car
+ {
+	 // Instance Variables
+	 // these are pieces of information that describe a car
+	 public String color;
+	 public int year;
+	 
+	 //Methods
+	 // These are things tha car can do (Actions)
+	 public void speak()
+	 {
+		 System.out.println("Vroom");
+	 }
+	 
+	 public String getColor()
+	 {
+		 return color ;
+	 }
+ }
