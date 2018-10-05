e82bd28ce81464b9b33979034e821cfe1d663716
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-10-04 19:54:52 -0700

    Add files via upload

--- Car2.java
+++ Car2.java
@@ -7,20 +7,22 @@ public class Car2
 		car1.vehicle= "Toyata Corolla ";
 		car1.gasTank= 13;
 		car1.fuelEfficiency= 30;
-		car1.gasTank*car1.fuelEfficiency;
+		
 		
 		Car2Specs car2= new Car2Specs();
 		car2.vehicle= "Ford F-150 ";
 		car2.gasTank= 30;
 		car2.fuelEfficiency= 20;
-		car2.gasTank*car2.fuelEfficiency;
+		
 		
 		Car2Specs car3= new Car2Specs();
 		car3.vehicle= "Toyata Prius ";
 		car3.gasTank= 9;
 		car3.fuelEfficiency= 45;
-		car3.gasTank*car3.fuelEfficiency;
-		
-		System.out.println(car1);
+
+		System.out.println(car1+"Can go this many miles "+ car1.drive());
+        System.out.println(car2+"Can go this many miles "+ car2.drive());
+        System.out.println(car3+"Can go this many miles "+ car3.drive());
+
 	}
 }
