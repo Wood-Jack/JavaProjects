bdd93f6059d68c3b7e9c9d386f4eeac0aaf460e8
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-10-08 18:55:30 -0700

    Add files via upload

---
+++ SuperStore2.java
@@ -0,0 +1,22 @@
+
+public class SuperStore2
+{
+	public static void main ( String [] args)
+	{
+		Product2 product1= new Product2("Acer Computer", 750.00, 700);
+		product1.name= "Acer computer ";
+		//product1.setName("Acer Computer");
+		//product1.setPrice (750.50);
+		//product1.setQuantity(500); 
+		
+		
+		Product2 product2= new Product2("Beats Headphones", 500.25, 400);
+		
+		Product2 product3= new Product2("Alien Ware", 25.00, 100);
+		
+		
+		product1.calcInventoryValue=  price*Quantity;
+		
+		System.out.println(product1);
+	}
+}
