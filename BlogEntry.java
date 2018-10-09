8c8a883bf14825daadeac85d7ae195efded75cdb
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-10-09 10:26:48 -0700

    Add files via upload

---
+++ BlogEntry.java
@@ -0,0 +1,24 @@
+
+public class BlogEntry
+{
+	//Instance Variables
+	public String userName,textEntry;
+	
+	public BlogEntry( String userName);
+	{
+		System.out.println(" Correct");
+		this.userName=userName;
+	}
+	
+	public BlogEntry (String userName, String textEntry);
+	{
+	     System.out.println(" Hurray");
+	     this.userName=userName;
+	     this.textEntry=textEntry;
+	 }
+	 public String DisplayEntry
+	 {
+		 return 
+}
+		
+
---
+++ BlogTester.java
@@ -0,0 +1,11 @@
+
+public class BlogTester
+{
+	public static void main ( String [] args)
+	{
+		BlogEntry blogEntry1= new BlogEntry( "Kevin Bacon", "Hello World")
+		
+		String 
+	}
+}
+		
