323d7b2d332680f61534401d25d9fca9c9773c78
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-10-18 10:54:55 -0700

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,33 @@
 
-public class HomeWork8_2
+
+
+public class Fraction
 {
-	public class main
+	// instance variables
+	private double numerator ;
+	private double denominator;
+	private double fractionDivision;
+	
+	// Constructor Method
+	public Fraction ( double numerator, double denominator, double fractionDivision)
+	{
+		this.numerator=numerator;
+		this.denominator=denominator;
+		this.fractionDivision= numerator/denominator;
+	}
+	
+	// get/ set method
+	
+	public double getNumerator()
+	{
+		return numerator;
+	}
+	public void setNumerator ( double numerator)
+	{
+		this.numerator= numerator;
+	}
+	public double getDenominator()
+}
+
+	
+	
---
+++ FractionTester.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+
+public class FractionTester
+{
+	public static void main (String args [])
+	{
+		Scanner fraction= new Scanner (System.in);
+		Fraction input1= new Fraction(7000,800,9999);
+		
+		
+		
+		Fraction input2= new Fraction(9999,9999,1000);
+		
+		System.out.println("please enter the Numerator ");
+		input1.setNumerator(double) = fraction.nextDouble();
+		
+		System.out.println(" Please enter the Denominator ");
+		input1.denominator = fraction.nextDouble();
+		
+	}
+}
