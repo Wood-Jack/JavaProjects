8f0ca75818ac2b7f8eac0fc2eef935c9571b4ee2
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-10-26 12:45:34 -0700

    Add files via upload

---
+++ HomeWork10.java
@@ -0,0 +1,80 @@
+
+
+
+import java.util.Scanner;
+
+public class HomeWork10
+{
+	public static void main (String [] args)
+	{
+		
+		/*Menu System
+		 * (1) add a new player and score ( up to 10 players)
+		 * (1) Print all the players scores
+		 * (1) find player by name and print
+		 * (1) remove player by name
+		 * (5) Quit
+		 * ********** */
+		 
+		 // input for menu options
+		System.out.println(" hello world");
+		int menuChoice1;
+		Scanner input= new Scanner(System.in);
+		
+		String playerNames[] = new String [10];
+		int playerScores[] = new int [10];
+		
+		int countPlayers=0;
+		
+		
+		do
+	{
+		System.out.println(" Choose a menu option");
+		System.out.println(" (1) add a new player and score");
+	    System.out.println(" (2) print all the players scores");
+		System.out.println(" (3) find player by name and print score");
+		System.out.println(" (4) Remove player by name");
+		System.out.println(" (5) Quit");
+		
+		menuChoice1= input.nextInt();
+		
+		System.out.print("Debug: you choose option;" + menuChoice1);
+		
+		// adding a new player name and scor
+		if (menuChoice ==1)
+		{
+			if(countPlayer >= 10)
+			{
+				System.out.println(" Already enterd 10 players");
+			}
+			else
+			{
+			Scanner inputName = new Scanner(System.in);
+			System.out.println("please enter Player Name:");
+			String playerName= playerInput.nextLine();
+			
+			//get player Score
+			System.out.println(" please enter player score ");
+			int playerScore= playerInput.nextInt();
+			
+			//assign values to array at correct index position
+			playerNames[countPlayers]= playerName;
+			playerScores[countPlayers]= playerScore;
+			countPlayers +=1;
+		}
+			
+			
+				
+			
+		}
+		//print all the player scores
+		else if (menuChoice1==2)
+		{
+		for ( int i= 0 ; i < countPlayers; i++)
+		{
+					System.out.println("Player" + i+ ":" + playerNames[i] + " has a score of " + playerScores[i]);
+		}
+	}
+	while  (menuChoice != 5);
+}
+		
---
+++ Week10.java
@@ -0,0 +1,30 @@
+
+public class Week10
+{ 
+	public static void main (String[] args)
+	{
+		/*
+		//Declaoring array of intgers of 30
+		int[] numberList= new int [30];
+		
+	
+	
+	
+		/*
+		// set values in the array
+		for (int i= 0; i < numberList.length; i++)
+		{
+			numberList[i]=i;
+		
+		
+		// Print values in the array
+		for ( int i=  0 ; i < numberList.length; i++)
+		{ 
+			System.out.println(" numberlist [" +i+ "] =" + numberList[i]);
+		}
+      }
+  }
+}
+*/
+}
+}
