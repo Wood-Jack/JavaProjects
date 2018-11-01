2ffbbc16731fce5ae7b948bee4c91d9cfec70498
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-11-01 11:11:01 -0700

    Add files via upload

--- HomeWork10_1.java
+++ HomeWork10_1.java
@@ -9,24 +9,24 @@ public class HomeWork10_1
 		Scanner myScanner= new Scanner (System.in);
 		
 		String [] months;
-		months = new String[13];
-		months[0]= null;
-		months[1]= "(1)January";
-		months[2]= "(2)Feburary";
-		months[3]= "(3)March";
-		months[4]= "(4)April";
-		months[5]= "(5)May";
-		months[6]= "(6)June";
-		months[7]= "(7)July";
-		months[8]= "(8)August";
-		months[9]= "(9)Septmeber";
-		months[10]="(10)October";
-		months[11]="(11)November";
-		months[12]="(12)December";
+		months = new String[12];
+		months[0]= "(1)January ";
+		months[1]= "(2)Feburary ";
+		months[2]= "(3)March ";
+		months[3]= "(4)April ";
+		months[4]= "(5)May ";
+		months[5]= "(6)June ";
+		months[6]= "(7)July ";
+		months[7]= "(8)August ";
+		months[8]= "(9)Septmeber ";
+		months[9]="(10)October ";
+		months[10]="(11)November ";
+		months[11]="(12)December ";
+		
 		
 		int [] rainFall;
-		rainFall= new int[13];
-		rainFall[0]= -1;
+		rainFall= new int[12];
+		rainFall[0]= 0;
 		rainFall[1]= 2;
 		rainFall[2]= 23;
 		rainFall[3]= 13;
@@ -38,18 +38,46 @@ public class HomeWork10_1
 		rainFall[9]= 11;
 		rainFall[10]= 22;
 		rainFall[11]= 1;
-		rainFall[12]= 0;
+		
+		
+		int [] inputRainFall;
+		inputRainFall= new int [13];
+		
+		int monthPicker;
+		int numberPicker;
+		
 		
 		for ( int i =0 ; i < months.length; i++)
 		{
 			System.out.println( months[i]);
 		} 
 		
+		do
+		{
 		System.out.println(" Please choose a number for the month");
-		int monthPicker = myScanner.nextInt();
+		monthPicker = myScanner.nextInt();
 		
-		//if (monthPicker ==1)
-
-}
+	
 		
+		
+			
+			for( int i = monthPicker; i < monthPicker + 12; i++)
+				{
+					System.out.println(" Please choose the a number for rainfall of  month" + months[i%12]);
+					numberPicker = myScanner.nextInt();
+					inputRainFall[i%12]=numberPicker;
+				}
+					
+					
+		//System.out.println("Your month" + months[monthPicker] + '\t' +" Your averager Rainfall" + rainFall[numberPicker]+ " Month Average RainFall" +rainfall);
+		
+				
+			
+		}
+		while (monthPicker == 13);
+				System.out.println( "Program exiting");
+				System.exit(13);
+			
+		
+	}
 }
