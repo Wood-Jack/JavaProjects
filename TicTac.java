ebcfbfa2a87c5ff64805cac006540e00d5bc951b
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-11-02 12:12:20 -0700

    Add files via upload

---
+++ TicTac.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class TicTac
+{
+	public static void main (String [] args )
+	{
+		Scanner playerInput= new Scanner (System.in)
+		
+		
+		// Tic tac table
+		int [][]table= [3][3]
+		
+		System.out.println("Tic- Tac program begin");
+		
+		for ( int row = 0 ; row < table.length; row++)
+		{
+			for(column= 0 ; column < table[row].length; column++
+			{
---
+++ Week11.java
@@ -0,0 +1,53 @@
+/*
+ * 
+ * 
+ * 
+ * Inside class work
+ * ***************** */
+ 
+ public class Week11
+ {
+	 public static void main (String [] args)
+	 {
+		 System.out.println( "hello world");
+		
+		
+		// Declare array
+		int[][] table = new int[6][6];
+		
+		//this our counter- it will start at 1 and go to 36
+		int number=1;
+		
+	
+		
+		
+			
+		
+		
+		//T0-Do: Assign values to each element of the array, 1-36
+		// Outer Loop: Loop through each ROW	
+		for (int row = 0 ; row < table.length; row++)
+		{	
+			// Inner Loop: For each row, loop through each column
+			for  ( int column =0 ; column < table[row].length ; column ++)
+			{
+				// Assign a value  to the array element
+				table[row][column] = number;
+				number++;
+			}
+		
+		}
+		for (int row = 0 ; row < table.length; row++)
+		{	
+			// Inner Loop: For each row, loop through each column
+			for  ( int column =0 ; column < table[row].length ; column ++)
+			{
+				// A display the value
+				System.out.print(table[row][column] + " ");
+				
+			}
+			// at the end of each row, print a new line
+			System.out.println();
+		}
+	}
+}
