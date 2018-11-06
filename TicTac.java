9e1f8f590da954a947f4a37b9f94c862a4762578
Wood-Jack <38797977+Wood-Jack@users.noreply.github.com>
2018-11-06 12:05:03 -0800

    Add files via upload

--- TicTac.java
+++ TicTac.java
@@ -10,31 +10,219 @@ public class TicTac
 		
 		
 		// Tic tac table
-		int [][] table= new int [3][3];
-		int number=1;
-		
-		
+		char [][] table = new char [3][3];
+		char player = 'X';
+		char ch = '1';
+		int row, column;
+		int turnCounter= 1;
 		
 		
 		System.out.println("Tic- Tac program begin");
 		
-			for ( int row = 0 ; row < table.length; row++)
+			for ( int i = 0 ; i < table.length; i++)
 			{
-				for(int column= 0 ; column < table[row].length; column++)
-			{
-				table[row][column]= number;
-				number++;
+				for(int j= 0 ; j <  table[i].length; j++)
+					{
+		                table[i][j]=ch;
+		                ch++;
+					 }
+				
 			}
-			}
-		
-			for ( int row = 0 ; row < table.length; row++)
+			for ( int i = 0 ; i < 3; i++)
 			{
-				for(int column= 0 ; column < table[row].length; column++)
-				{
-				System.out.print(table[row][column]+ "  ");
-				}
+				for(int j= 0 ; j <  3; j++)
+					{
+		                System.out.print(table[i][j]);
+					 }
 				System.out.println();
 			}
-		System.out.println("please enter number");
+			
+			
+			while(winner(table) ==false && turnCounter !=9)
+			{
+				System.out.println( "Enter a postion for player" + player);
+				row = playerInput.nextInt();
+				
+				
+			
+				while(!markTable(row,table,player))
+				{
+					System.out.println(" This spot is taken. Try again");
+					System.out.println( "Enter a another postion" + player);
+				row = playerInput.nextInt();
+				}
+			
+		
+				
+				
+				for ( int i = 0 ; i < 3; i++)
+				{
+				for(int j= 0 ; j <  3; j++)
+					{
+		                System.out.print(table[i][j]);
+					 }
+				System.out.println();
+				}
+			
+				
+				
+				
+				if (winner(table))
+				{
+					System.out.println("Player" + player+ " is the winner!!!");
+				}
+				
+				// player taking turns
+				turnCounter++;
+				
+				if (player== 'O')
+				{
+					player= 'X';
+				}
+				else
+				{
+					player = 'O';
+				}
+				if (winner(table) == false && turnCounter == 9 )
+				{
+					System.out.println("Game is a tie");
+				}
+				
+		}
+		
 	}
+	public static  boolean markTable(int input, char [][] table, char symbol)
+	{ 
+	
+	
+		if (input== 1)
+		{
+			if(table[0][0]== 'X' || table[0][0]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[0][0]= symbol;
+				 return true;
+			}
+			
+		}
+		else if (input==2)
+		{
+			
+			if(table[0][1]== 'X' || table[0][1]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[0][1]= symbol;
+				 return true;
+			}
+		}
+		else if (input==3)
+		{
+			
+			if(table[0][2]== 'X' || table[0][2]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[0][2]= symbol;
+				 return true;
+			}
+		}
+		else if (input==4)
+		{
+			
+			if(table[1][0]== 'X' || table[1][0]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[1][0]= symbol;
+				 return true;
+			}
+		}
+		else if (input==5)
+		{
+			
+			if(table[1][1]== 'X' || table[1][1]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[1][1]= symbol;
+				 return true;
+			}
+		}
+		else if (input == 6)
+		{
+			
+			if(table[1][2]== 'X' || table[1][2]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[1][2]= symbol;
+				 return true;
+			}
+		}
+		else if (input == 7)
+		{
+			
+			if(table[2][0]== 'X' || table[2][0]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[2][0]= symbol;
+				 return true;
+			}
+		}
+		else if (input == 8)
+		{
+			
+			if(table[2][1]== 'X' || table[2][1]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[2][1]= symbol;
+				 return true;
+			}
+		}
+		else if (input ==9)
+		{
+			
+			if(table[2][2]== 'X' || table[2][2]== 'O')
+			{
+				return false;
+			}
+			else
+			{
+				 table[2][2]= symbol;
+				 return true;
+			}
+		}
+		
+		
+	return false;
+
+	}
+	
+	public static boolean winner(char [][]table)
+			{
+				return ( (table[0][0] == table[1][0] && table[0][0]== table[2][0]) || (table[0][0] == table[0][1]&& table[0][0] == table[0][2] )||
+				(table[1][0] == table[0][1] && table[1][0] == table[0][2]) || (table[2][0] == table[2][1] && table[2][0] == table[2][2])
+				||(table[0][0]== table[1][1] && table[0][0] == table[2][2]) || (table[0][0]== table[1][1] && table[0][0] == table[2][1])
+				||(table[0][2]== table[1][2] && table[0][2] == table[2][2])|| (table[2][0] == table[1][1]&& table [2][0] == table[0][2]));
+			}
 }
