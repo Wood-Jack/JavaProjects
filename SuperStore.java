2ed9a4989d635a2293ab6e530feacc3ca3c6b9ef
Woodrow Jackson <38797977+Wood-Jack@users.noreply.github.com>
2022-08-16 11:56:27 -0400

    Update and rename SuperStore3.java to SuperStore.java

--- SuperStore.java
+++ SuperStore.java
@@ -3,19 +3,19 @@ public class SuperStore3
 {
 	public static void main ( String [] args)
 	{
-		Product3 product1= new Product3("Acer Computer", 750.00, 700);
+		Product product1= new Product("Acer Computer", 750.00, 700);
 		//product1.name= "Acer computer ";
 		product1.setName("Acer Computer");
 		product1.setPrice (750.50);
 		product1.setQuantity(500); 
         
 		
-		Product3 product2= new Product3("Beats Headphones", 500.25, 400);
+		Product product2= new Product("Beats Headphones", 500.25, 400);
 		product2.setName("Beats Headphones");
 		product2.setPrice(500.25);
 		product2.setQuantity(400);
 		
-		Product3 product3= new Product3("Alien Ware", 25.00, 100);
+		Product product3= new Product3("Alien Ware", 25.00, 100);
 		product3.setName("Alien Ware");
 		product3.setPrice(25.00);
 		product3.setQuantity(100);
